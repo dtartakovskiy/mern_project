d911515ceb1b5a1967a3e1a63b264a7563425f32
Dmytro Tartakovskyi <d.tartakovskiy@gmail.com>
2021-12-31 14:59:10 +0200

    page progress

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import Post from '../models/Post.js'
 
 // Errors https://www.restapitutorial.com/httpstatuscodes.html
@@ -5,7 +6,7 @@ import Post from '../models/Post.js'
 export const getPosts = async (req, res) => {
   try {
     const posts = await Post.find({})
-    res.status(200).json({ posts })
+    res.status(200).json(posts)
   } catch (error) {
     res.status(404).json({ msg: error.message })
   }
@@ -15,6 +16,21 @@ export const createPost = async (req, res) => {
   try {
     const post = await Post.create({ ...req.body })
     res.status(201).json(post)
+  } catch (error) {
+    res.status(409).json({ msg: error.message })
+  }
+}
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params
+  const { body } = req.body
+  if (!mongoose.isValidObjectId(_id)) {
+    return res.status(404).send('No post with that id')
+  }
+
+  try {
+    const { data } = await Post.findByIdAndUpdate(_id, body, { new: true })
+    res.status(204).json({ data })
   } catch (error) {
     res.status(409).json({ msg: error.message })
   }
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
 
+import postsRouter from './routes/posts.js'
 import connectDB from './helpers/connectDB.js'
 
 const app = express()
@@ -12,6 +13,8 @@ const app = express()
 app.use(express.json({ limit: '30mb' }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
+
+app.use('/posts', postsRouter)
 
 const PORT = process.env.PORT || 5000
 
